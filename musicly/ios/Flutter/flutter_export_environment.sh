0502a287560e50eb4574f7003d95f4ef5d46e76c
Yashank <yashank.cronaldo@gmail.com>
2020-05-10 19:28:22 -0700

    Init

---
+++ musicly/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/mnt/82F2EACCF2EAC38F/stickyburn/flutter/xx_flutter_lib"
+export "FLUTTER_APPLICATION_PATH=/mnt/82F2EACCF2EAC38F/stickyburn/flutter/flutterCoding/musicly"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/mnt/82F2EACCF2EAC38F/stickyburn/flutter/xx_flutter_lib/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
